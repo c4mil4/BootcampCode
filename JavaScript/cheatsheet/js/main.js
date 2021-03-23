552c3b6b0ee40ef891f31a7f61a56207f6d6051e
Martinez Camila <martinezce_@hotmail.com>
2021-03-23 18:07:36 +0200

    js

--- JavaScript/cheatsheet/js/main.js
+++ JavaScript/cheatsheet/js/main.js
@@ -1,167 +1,168 @@
 // ----------uso de js externo----------
 
-function saludo () {
+function saludo() {
     alert("Hola desde un script externo");
     console.log("has hecho click");
-    }
-    // document.getElementsByTagName("a").item(2).addEventListener("click", saludo);
+}
+// document.getElementsByTagName("a").item(2).addEventListener("click", saludo);
 
-    // ----------Depuracion / logging----------
-    // alert("Mensaje");
-    console.log("");
+// ----------Depuracion / logging----------
+// alert("Mensaje");
+console.log("");
 
 
-    // ----------Variables---------
-    // var-> function scope
-    // let / const -> block scope
+// ----------Variables---------
+// var-> function scope
+// let / const -> block scope
 
-    function saludar( ) {
-        console.log("Inicial: ", contador);
+function saludar() {
+    console.log("Inicial: ", contador);
 
-        // Comprobar con var y con let y ver diferencias
-        for (var contador = 0; contador < 6; contador++){
+    // Comprobar con var y con let y ver diferencias
+    for (var contador = 0; contador < 6; contador++) {
         console.log(contador);
-        }
-        console.log("Final: ", contador);
     }
+    console.log("Final: ", contador);
+}
 
-    saludar();
+saludar();
 
-    console.clear();
-    // Inicializacion
-    let day;
-    day = 5;
+console.clear();
+// Inicializacion
+let day;
+day = 5;
 
-    let monthName = "Octubre";
+let monthName = "Octubre";
 
-    let hour, minute, second;
+let hour, minute, second;
 
-    let array = [15, 20, 25]
-    console.log(array[0], array[1], array[2]); 
-    let [var1, var2, var3] = [15, 20, 25]; //AVANZADA
-    console.log(var1, var2,var3);
+let array = [15, 20, 25]
+console.log(array[0], array[1], array[2]);
+let [var1, var2, var3] = [15, 20, 25]; //AVANZADA
+console.log(var1, var2, var3);
 
-    // const -> Block Scope
-    const x=1;
-    // x = 2 ERROR UNA CONSTANTE NO PUEDE SER SOBREESCRITA
-    // const y; error missing = in const
+// const -> Block Scope
+const x = 1;
+// x = 2 ERROR UNA CONSTANTE NO PUEDE SER SOBREESCRITA
+// const y; error missing = in const
 
-    
 
-    // ---------- Tipos de Datos ----------
-    
-    
-    // ----------Primitivos----------
-    let str = String("Im a string");
-    let str2 = "Im another string"; // Recomendado  
-    console.log(typeof str, typeof str2, typeof "");
 
-    // let str = "string" ES IGUAL A "string"
+// ---------- Tipos de Datos ----------
 
-    let num = Number (5);
-    let num2 = 5;
-    console.log(typeof num, typeof num2, typeof 5, typeof 5.3, typeof"5");
 
-    let bool = Boolean(true);
-    let bool2 = false;
-    console.log(typeof bool, typeof bool2, typeof true, typeof "true");
+// ----------Primitivos----------
+let str = String("Im a string");
+let str2 = "Im another string"; // Recomendado  
+console.log(typeof str, typeof str2, typeof "");
 
-    let undef;
-    console.log(undef, typeof undef);
+// let str = "string" ES IGUAL A "string"
 
-    let selectedColor = null; //Limpiar una variable
-    console.log(selectedColor, typeof selectedColor);
+let num = Number(5);
+let num2 = 5;
+console.log(typeof num, typeof num2, typeof 5, typeof 5.3, typeof "5");
 
-    // ----------Objetos----------
-    array = Array(1, 2, 3);
-    let array2 = [1, 2, 3];
-    console.log(array,array2);
-    console.log(typeof array, typeof array2);
+let bool = Boolean(true);
+let bool2 = false;
+console.log(typeof bool, typeof bool2, typeof true, typeof "true");
 
-    console.log(typeof new Date("2021-05-21"));
-    console.log(typeof new Error("Mensaje de Error"));
+let undef;
+console.log(undef, typeof undef);
 
-    console.clear();
-    // ----------Strings----------
-    str = "Hola Futuro";
-    console.log(str, str.length);
-    console.log(str.indexOf("Futuro"), str.indexOf("Cadena no Presente"));
-    console.log("La ultima ocurrecia de `u` esta en el indice", str.lastIndexOf("u"));
+let selectedColor = null; //Limpiar una variable
+console.log(selectedColor, typeof selectedColor);
 
-    console.log(str.substring(1, 3));
+// ----------Objetos----------
+array = Array(1, 2, 3);
+let array2 = [1, 2, 3];
+console.log(array, array2);
+console.log(typeof array, typeof array2);
 
-    console.log("Hola mundo".concat(" .Adios mundo"));
-    console.log("Hola mundo" + ".Adios mundo."); //Equivalente a concat
-    str = str + ". Adios mundo.";
-    str += ". Adios mundo.";
-    console.log(str);
-    console.log(str.replace("futuro", "mundo"));
-    console.log(str.replaceAll("mundo", "futuro"));
-    console.log(str.toLocaleLowerCase());
-    console.log(str.toUpperCase());
+console.log(typeof new Date("2021-05-21"));
+console.log(typeof new Error("Mensaje de Error"));
 
-    console.log(`El dia de mi cumpleaños es ${day}`); //String template
-    console.log("El dia de mi cumpleaños es", day);
+console.clear();
+// ----------Strings----------
+/*  str = "Hola Futuro";
+ console.log(str, str.length);
+ console.log(str.indexOf("Futuro"), str.indexOf("Cadena no Presente"));
+ console.log("La ultima ocurrecia de `u` esta en el indice", str.lastIndexOf("u"));
 
-    //----------Parsing ( conversion de un tipo a otro )----------
-    let number = Number("asdasd"); // Nan = Not a Number. No genera error.
-    console.log(number, typeof number);
+ console.log(str.substring(1, 3));
 
-    number = Number("1000");
-    console.log(number, typeof number);
+ console.log("Hola mundo".concat(" .Adios mundo"));
+ console.log("Hola mundo" + ".Adios mundo."); //Equivalente a concat
+ str = str + ". Adios mundo.";
+ str += ". Adios mundo.";
+ console.log(str);
+ console.log(str.replace("futuro", "mundo"));
+ console.log(str.replaceAll("mundo", "futuro"));
+ console.log(str.toLocaleLowerCase());
+ console.log(str.toUpperCase());
 
-    number+= "1000"; // Entiende que es una concatenacion, no una suma
-    console.log(number, typeof number);
+ console.log(`El dia de mi cumpleaños es ${day}`); //String template
+ console.log("El dia de mi cumpleaños es", day); */
 
-    console.log("1" + 1, "1" - 1);// con el + contaquena, el - no tiene otro valor que no sea restar 
+//----------Parsing ( conversion de un tipo a otro )----------
+let number = Number("asdasd"); // Nan = Not a Number. No genera error.
+console.log(number, typeof number);
 
-    // ----------Objetos----------
-    let person = {
-        name: "Oscar",
-        age: 30,
-        blonde: false,
-        jump() {
-            console.log("Hey, mira como salto!!");
-        }//Funcion 
-    }
+number = Number("1000");
+console.log(number, typeof number);
 
-    console.log(person);
-    console.log(`Se llama ${person.name} y tiene ${person.age} años`);
-    
-    person.name = "Dimitrov";
-    // Acceso por punto / Dot notation
+number += "1000"; // Entiende que es una concatenacion, no una suma
+console.log(number, typeof number);
 
-    console.log(person, typeof person);
-    console.log(`Se llama ${person.name} y tiene ${person.age} años`);
+console.log("1" + 1, "1" - 1);// con el + concatena, el - no tiene otro valor que no sea restar 
 
-    // Acceso por corchetes / Brackets notation
+// ----------Objetos----------
+let person = {
+    name: "Oscar",
+    age: 30,
+    blonde: false,
+    jump() {
+        console.log("Hey, mira como salto!!");
+    },//Funcion 
 
-    let propertyName = "age";
-    console.log(person[propertyName]);
+}
 
-    //Ampliacion
+console.log(person);
+console.log(`Se llama ${person.name} y tiene ${person.age} años`);
 
-    person.children = ["Marta","Peter"];
-    console.log(person);
-    console.log(person.children);
+person.name = "Dimitrov";
+// Acceso por punto / Dot notation
 
-    person.jump();
+console.log(person, typeof person);
+console.log(`Se llama ${person.name} y tiene ${person.age} años`);
 
-    // {} Objeto, [] Array tambien es objeto, "" String
-    console.log(typeof{}, typeof [], typeof"");
+// Acceso por corchetes / Brackets notation
+
+let propertyName = "age";
+console.log(person[propertyName]);
+
+//Ampliacion
+
+person.children = ["Marta", "Peter"];
+console.log(person);
+console.log(person.children);
+
+person.jump();
+
+// {} Objeto, [] Array tambien es objeto, "" String
+console.log(typeof {}, typeof [], typeof "");
 
 
 // ----------This-----------
 
-const person2= {
-    
+const person2 = {
+
     name: "Thom",
     talk() {
         console.log(`Me llamo ${this.name}`);
     }
 }
 
-const person3= {
+const person3 = {
     name: "Sarah",
     talk() {
         console.log(this);
@@ -178,7 +179,7 @@ console.clear();
 let selectedColors = ["red", "blue"]; // Array de String
 console.log(selectedColors, selectedColors.length, typeof selectedColors);
 
-selectedColors[2] = "green"; 
+selectedColors[2] = "green";
 console.log(selectedColors, selectedColors.length);
 
 selectedColors.push("voileta"); //RECOMENDADA
@@ -193,7 +194,7 @@ console.log(selectedColors, selectedColors.length);
 selectedColors.shift("white");
 console.log(selectedColors, selectedColors.length);
 
-selectedColors.sort();
+selectedColors.sort(); //pertenece al prototipo de arrays
 console.log(selectedColors, selectedColors.length);
 
 console.log("El rojo esta en el indice ", selectedColors.indexOf("red"));
@@ -214,7 +215,7 @@ let chico = {
     name: "Oscar",
     height: 1.36,
     genero: "masculino",
-    amigos:[],
+    amigos: [],
     perderAmigo() {
         this.amigos.pop()
     }
@@ -223,7 +224,7 @@ let chico = {
 
 console.log(chico);
 
-chico.amigos.push("cristian","luisito","tania") // para añadir push anque el array este vacio 
+chico.amigos.push("cristian", "luisito", "tania") // para añadir push aunque el array este vacio 
 console.log(chico.amigos);
 
 chico.amigos.unshift("manuel");
@@ -232,19 +233,132 @@ console.log(chico.amigos);
 chico.perderAmigo();
 console.log(chico.amigos);
 
-// ----------Condicionales----------
+// ----------Condicionales(bloques de flujo, sino)----------
 
-const randonNumber = 9;
-const quessedNumber = "9";
-if(typeof randonNumber !== typeof quessedNumber){
+const randonNumber = 5;
+const guessedNumber = "5";
+if (typeof randonNumber !== typeof guessedNumber) {
     console.log("Hey tienen diferente clase");
 }
 
-if (randonNumber === quessedNumber) {
-    console.log("Has acertado el numero");
-} else if (randonNumber > quessedNumber) {
-    console.log("El numero secreto es mayor");
-} else if (randonNumber < quessedNumber) {
-    console.log("El numero secreto es menor");
+if (typeof guessNumber === "string") {
+    console.log("");
 }
 
+if (randonNumber === guessedNumber) {
+    console.log("Has acertado el numero");
+} else if (randonNumber > guessedNumber) {
+    console.log("El numero secreto es mayor");
+} else if (randonNumber < guessedNumber) {
+    console.log("El numero secreto es menor");
+} 
+
+// Ternary operator 
+
+let variable = 12 < 10 ? "el primero es menor" : "el primero es mayor";
+console.log(variable);
+
+//switch 
+let option = 5;
+switch (option) {
+    case 1:
+        //bloque de codigo para valor 1
+        console.log("option vale 1");
+        break;
+   case 2:
+        //bloque de codigo para valor 
+        console.log("option vale 2");
+        break;
+    case 3:
+        //bloque de codigo para valor 3
+        console.log("option vale 1");
+        break;
+    default: //"de otro modo" de pseint
+        console.log("otra opcion");
+        break;
+}
+
+console.clear();
+ // funciones
+ // nombradas
+ function greet(name, lastName) {
+     console.log(`hola, ${name} ${lastName}. ¿Que tal?`);
+ }
+
+greet();
+greet("Marcos", "Aurelio");
+//greet("Marcos", "aurelio");
+
+//que devuelva el cuadrado de un numero que recibe un parametro
+ function cuadrado (number){
+    let cuadrado = number1 * number1;
+    return cuadrado;
+ }
+ console.log(cuadrado(5));
+
+ //anonimas 
+
+ let numbersArray = [5, 51, 1, 15, 2]
+ console.log(numbersArray);
+ numbersArray.sort(); // lo ordena segun ASCII
+ console.log(numbersArray);
+
+ //se aplica en arrayas con numeros, objetos.
+
+function orderNumbers (a, b) {
+    if(a < b) {
+        return -1;
+    } else if (a === b) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+ //numbersArray.sort(orderNumbers);
+
+ numbersArray.sort(function orderNumbers (a, b) {
+    if(a < b) {
+        return -1;
+    } else if (a === b) {
+        return 0;
+    } else {
+        return 1;
+    }
+});
+
+numbersArray.sort(function (a, b){return a - b});
+console.log(typeof orderNumbers,typeof function (a, b) {return a - b});
+console.log(numbersArray);
+
+console.clear();
+// arrow fuctuions
+let perimeterOfSquare = function (side) { // Función anónima normal
+    return side * 4;
+    }
+    perimeterOfSquare = function (side) {return side * 4;} // (opcional) Una única línea
+    perimeterOfSquare = (side) => {return side * 4;} // Sustituyo function por la flecha después de params.
+    perimeterOfSquare = (side) => side * 4; // Si sólo quiero devolver algo, quito llaves y return.
+    perimeterOfSquare = side => side * 4; // Si sólo tiene 1 param, puedo quitar paréntesis.
+    
+    console.log(perimeterOfSquare(5));
+
+//------------ for
+for (let i = 0; i <= 10; i++) {
+    console.log(`Indice: ${i}`);
+
+}
+// definir y rellenar un array con el indice i
+const arrayX = [];
+
+for (let i = 0; i <= 10; i++) {
+    arrayX.push(i);
+}
+
+console.log(arrayX);
+
+//while 
+let contador = 0;
+while (contador <= 10 ) {
+    console.log(contador)
+}
